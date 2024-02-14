784a5e8f84f2f2e24a15c5f4898e50bfdad59cdd
Abhi200206 <120104020+Abhi200206@users.noreply.github.com>
2024-02-14 19:11:49 +0530

    ok

---
+++ src/components/Para.jsx
@@ -0,0 +1,6 @@
+export default function Para(props)
+{
+    return (
+        <div><p className='font-sans font-[700] text-[22px] leading-[42.24px] tracking-[-0.32px] pl-[8px] '>{props.p}</p></div>
+    )
+}
