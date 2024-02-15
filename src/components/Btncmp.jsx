203e443524fe5ffd07838b8b2af30f714e6e646d
Abhi200206 <120104020+Abhi200206@users.noreply.github.com>
2024-02-15 22:16:16 +0530

    all ok

--- src/components/Btncmp.jsx
+++ src/components/Btncmp.jsx
@@ -1,13 +1,13 @@
 export default function Btncmp(props)
 {
     return (
-        <div className="border-[1px] border-[]">
+        <div className="border-[1px] md:py-6">
         <div className="m-2  ">
             <div className="flex gap-2">
-            <input type="checkbox" /> <p>{props.p}</p></div>
+            <input type="checkbox" /> <p className={props.styl}>{props.p}</p></div>
             <div className="flex justify-end cursor-pointer">
             <div className="bg-[#4375FB] text-center px-2 w-[100px] flex justify-end ">
-                <p className="text-white  p-2">{props.value}</p>
+                <p className="text-[#F2F2F2] text-[12px]  p-2">{props.value}</p>
             </div>
             </div>
         </div>
--- src/components/Para.jsx
+++ src/components/Para.jsx
@@ -1,6 +1,6 @@
 export default function Para(props)
 {
     return (
-        <div><p className='font-sans font-[700] text-[22px] leading-[42.24px] tracking-[-0.32px] pl-[8px] '>{props.p}</p></div>
+        <div><p className='md:text-[32px] font-sans font-[700] text-[22px] leading-[42.24px] tracking-[-0.32px] pl-[8px] '>{props.p}</p></div>
     )
 }
